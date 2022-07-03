69985e4930485f4728c904b4eac4823817a0216f
sorawit <sorawit.tr@gmail.com>
2022-07-03 18:45:10 +0700

    first commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Sorawit Trutsat on 3/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
